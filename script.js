4517d0afed26ec0c9c97c8ba9fe259bcd4bfbd7d
Deesjes <85071119+Deesjes@users.noreply.github.com>
2021-07-07 21:17:14 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hallo Winc Academy studenten")
+let name = 'Dit is een grote som'
+console.log("name")
+let multiplication = 1230941 * 1823794;
+console.log("multiplication:", multiplication);
+let subtraction = 637263 - 54;
+console.log("subtraction:", subtraction);
